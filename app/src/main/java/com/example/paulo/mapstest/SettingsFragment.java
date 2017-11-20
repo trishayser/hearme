7a2d4fb6900d158406cd768b37b378a5818eebb9
Tristan <me@tristanhsr.de>
2017-11-20 23:43:50 +0100

    fuege "Einstellungen" hinzu

--- app/src/main/java/com/example/paulo/mapstest/SettingsFragment.java
+++ app/src/main/java/com/example/paulo/mapstest/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.paulo.mapstest;
 
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
         import android.os.Bundle;
         import android.view.LayoutInflater;
@@ -7,7 +8,8 @@ import android.support.v4.app.Fragment;
         import android.view.ViewGroup;
 
 
-public class SettingsFragment extends Fragment {
+//public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
 
     public SettingsFragment() {
     }
@@ -16,7 +18,12 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.settings, container, false);
+        addPreferencesFromResource(R.xml.pref_headers);
+
 
         return rootView;
     }
+
+
+
 }
