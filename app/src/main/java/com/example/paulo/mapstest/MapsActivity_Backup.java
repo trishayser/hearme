4659612c8fdacda2b9b5fcbc7269ea180e307c3f
pascal <pascal-piper@gmx.de>
2017-11-23 23:40:25 +0100

    Mit Neuer Record Activity_2

---
+++ app/src/main/java/com/example/paulo/mapstest/MapsActivity_Backup.java
@@ -0,0 +1,69 @@
+//package com.example.paulo.mapstest;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.Menu;
+//import android.view.MenuInflater;
+//import android.view.MenuItem;
+//
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//
+//public class MapsActivity_Backup extends AppCompatActivity implements OnMapReadyCallback {
+//
+//    private GoogleMap mMap;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_maps_2);
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(0, 0);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        //mMap.setMyLocationEnabled(true);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
+//    // Edit Pascal
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) { //menu activity bekannt
+//         MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) { //click listener quasi.
+//
+//        switch (item.getItemId()) {
+//            case R.id.profil_change:
+//                System.out.println("Profil wurde geändert");
+//
+//                // Intent erzeugen und Starten der AktiendetailActivity mit explizitem Intent
+//                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+//               // settingsIntent.putExtra(Intent.EXTRA_TEXT, aktienInfo);
+//                startActivity(settingsIntent);
+//
+//                break;
+//        }
+//        return super.onOptionsItemSelected(item); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    // Edit Pascal Ende
+//}
---
+++ app/src/main/java/com/example/paulo/mapstest/NewRecordActivityActivity.java
@@ -0,0 +1,13 @@
+package com.example.paulo.mapstest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewRecordActivityActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_record_activity);
+    }
+}
