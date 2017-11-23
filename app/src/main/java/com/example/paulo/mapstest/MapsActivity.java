78bb2ddd7e85b5716305a93207e92966c8ccc633
pascal <pascal-piper@gmx.de>
2017-11-23 17:52:35 +0100

    Neue Main Activity XML

--- app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
@@ -24,7 +24,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps_2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
