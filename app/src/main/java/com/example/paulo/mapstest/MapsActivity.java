3a7d1d0ead5bb6d9832baffe2bcaeab773289358
pascal <pascal-piper@gmx.de>
2017-11-23 23:27:41 +0100

    Mit Neuer Record Activity

--- app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
@@ -1,34 +1,53 @@
 package com.example.paulo.mapstest;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import android.content.Intent;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps_2);
+        setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Neue Nachricht");
+
+                // Intent erzeugen und Starten der AktiendetailActivity mit explizitem Intent
+                Intent testIntent = new Intent(getApplication().getApplicationContext(), SettingsActivity.class);
+                // settingsIntent.putExtra(Intent.EXTRA_TEXT, aktienInfo);
+                startActivity(testIntent);
+
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
