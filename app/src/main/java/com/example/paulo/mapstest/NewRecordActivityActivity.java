c5d7ec0d1d89d0bba3b0efc543b3c0f437604272
pascal <pascal-piper@gmx.de>
2017-11-24 16:11:17 +0100

    viel geändert

--- app/src/main/java/com/example/paulo/mapstest/NewRecordActivityActivity.java
+++ app/src/main/java/com/example/paulo/mapstest/NewRecordActivityActivity.java
@@ -1,13 +1,28 @@
 package com.example.paulo.mapstest;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class NewRecordActivityActivity extends Activity {
+
+public class NewRecordActivityActivity extends AppCompatActivity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_record_activity);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Neue Nachricht");
     }
 }
