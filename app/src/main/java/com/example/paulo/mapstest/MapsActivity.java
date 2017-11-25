81afe59fb28f729d05922d1e541e12338e469f02
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2017-11-25 18:09:48 +0100

    just gucking

--- app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
@@ -69,13 +69,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(0, 0);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
         //mMap.setMyLocationEnabled(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
     // Edit Pascal
 
     @Override
