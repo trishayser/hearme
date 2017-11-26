22b9832d36d081eef002ac18844bb1a4b1856b33
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2017-11-26 21:11:45 +0100

    gps coordinates methods

--- app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/paulo/mapstest/MapsActivity.java
@@ -1,14 +1,24 @@
 package com.example.paulo.mapstest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +30,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private Button abschicken;
+    private TextView coordinates;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +60,71 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
+
+                abschicken = (Button) findViewById(R.id.abschicken);
+                coordinates = (TextView) findViewById(R.id.coordinates);
+
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        coordinates.append("\n" + location.getLatitude() + " " + location.getLongitude());
+
+                    }
+
+                    @Override
+                    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String s) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String s) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+
+                    }
+                };
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.INTERNET
+                    },10);
+                    return;
+                }
+                }else{
+                    configureButton();
+                }
+
+            }
+
+            //@Override
+            public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
+                switch (requestCode){
+                    case 10:
+                        if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                            configureButton();
+                        return;
+                }
+            }
+
+            private void configureButton() {
+                abschicken.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view) {
+
+                        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
+                    }
+                });
+
             }
         });
 
