f5f86ab51e01767d24bd0bdcf672aeed5c8584d1
pascal <pascal-piper@gmx.de>
2017-12-07 22:38:24 +0100

    registrations_Activity_2

---
+++ app/src/main/java/com/example/paulo/mapstest/LoginStart.java
@@ -0,0 +1,40 @@
+package com.example.paulo.mapstest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginStart extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_start);
+
+        final Button login = (Button) findViewById(R.id.anmelden);
+        final Intent start = new Intent(this ,MapsActivity.class);
+
+        final Button registration = (Button) findViewById(R.id.registrieren);
+        final Intent registrationIntent = new Intent(this ,Registration.class);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(start);
+            }
+        });
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(registrationIntent);
+            }
+        });
+
+
+    }
+}
