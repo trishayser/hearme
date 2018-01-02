c2a606b4c96367bd8a65c872b44a279581b967b8
pascal <pascal-piper@gmx.de>
2018-01-02 18:09:05 +0100

    new_record_activity

--- app/src/main/java/com/thkoeln/paulo/hearme/NewRecordActivityActivity.java
+++ app/src/main/java/com/thkoeln/paulo/hearme/NewRecordActivityActivity.java
@@ -223,11 +223,18 @@ public class NewRecordActivityActivity extends AppCompatActivity {
 
                 kommentar = (EditText) findViewById(R.id.comment);
 
-                double longitude = location().getLongitude ();
-                double latitude = location().getLatitude ();
+                if (location()==null){
+                    System.out.println("loc ist null");
+                    Toast.makeText(NewRecordActivityActivity.this, "Loc ist null", Toast.LENGTH_SHORT).show();
 
-                kommentar.setText(longitude +" " +latitude);
+                }
+                else {
 
+                    double longitude = location().getLongitude();
+                    double latitude = location().getLatitude();
+
+                    kommentar.setText(longitude + " " + latitude);
+                }
 
                 DatabaseReference mDatabase;
                 mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -320,10 +327,15 @@ public class NewRecordActivityActivity extends AppCompatActivity {
         criteria.setAltitudeRequired(true);
         criteria.setSpeedRequired(false);
 
-        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        String locationProvider = lm.NETWORK_PROVIDER;
         // Or use LocationManager.GPS_PROVIDER
 
         Location loc = lm.getLastKnownLocation(locationProvider);
+
+//        String bestLocationProvider = lm.getBestProvider (criteria,true );
+//        System.out.println(bestLocationProvider);
+//       // LocationProvider lp = lm.getProvider (bestLocationProvider);
+//        Location loc = lm.getLastKnownLocation (lm.NETWORK_PROVIDER);
 
         return loc;
     }
